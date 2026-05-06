37347642920f28d970246a44b89fcc9662b54788
prathmesh koli <prathmesh4730@gmail.com>
2026-05-06 13:08:44 +0530

    Create leetcode9.cpp

---
+++ leetcode9.cpp
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/maximum-subarray/description/*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cur = nums[0], best = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            cur = max(nums[i], cur + nums[i]);
+            best = max(best, cur);
+        }
+        return best;
+    }
+};
