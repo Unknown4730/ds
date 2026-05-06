494e99472ef31fa350e170e740cbe76f4aee9f21
prathmesh koli <prathmesh4730@gmail.com>
2026-05-06 13:02:38 +0530

    Create leetcode2.cpp

---
+++ leetcode2.cpp
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/contains-duplicate/description/ */
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int x : nums) {
+            if (st.count(x)) return true;
+            st.insert(x);
+        }
+        return false;
+    }
+};
