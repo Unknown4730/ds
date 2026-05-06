dfc784fb5719d96d0f96a8c62068614600cf1697
prathmesh koli <prathmesh4730@gmail.com>
2026-05-06 13:09:15 +0530

    Create leetcode10.cpp

---
+++ leetcode10.cpp
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/ */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn = INT_MAX, ans = 0;
+        for (int p : prices) {
+            mn = min(mn, p);
+            ans = max(ans, p - mn);
+        }
+        return ans;
+    }
+};
